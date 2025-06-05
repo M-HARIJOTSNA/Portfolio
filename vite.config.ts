c9a7bbfb5ed57ac69d1faceb99f823dfea89a2f5
Harijotsna Madala <harijotsnamadala@gmail.com>
2025-06-05 10:58:07 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',  // <<== IMPORTANT
+  base: '/Portfolio/',  // 👈 EXACTLY this
   plugins: [react()],
 });
