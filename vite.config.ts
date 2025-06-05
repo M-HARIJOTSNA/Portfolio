88bdf2230c1c8d7c8d1308c3a01f938aeb97b7ed
Harijotsna Madala <harijotsnamadala@gmail.com>
2025-06-05 10:53:34 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Portfolio/', // 👈 very important for GitHub Pages
+  base: './',  // <<== IMPORTANT
   plugins: [react()],
-})
+});
