bd6dd84fc9ce611da898c2bbd657f5eb7798a162
Lokesh <llokesh26204@gmail.com>
2025-06-15 14:31:35 +0530

    Let's see

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Portfolio/',  // 👈 EXACTLY this
   plugins: [react()],
+  base: '/portfolio/', // Set to '' if you're deploying to root
 });
